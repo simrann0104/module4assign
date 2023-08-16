5d14a973ca2b52957fb6ae6d28bdd1022b50b5a7
simrann0104 <137884800+simrann0104@users.noreply.github.com>
2023-08-16 14:32:20 +0530

    Create speakhello.js

---
+++ speakhello.js
@@ -0,0 +1,9 @@
+V(function (window) {  
+    var helloSpeaker = {};
+    var speakWord = "Hello";
+    
+    helloSpeaker.speak = function (names) {
+      console.log(speakWord + " " + names);
+    }
+        window.helloSpeaker = helloSpeaker;
+    })(window);
